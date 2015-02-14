37665ed75f13becd70c50459952003e2903e2d36
Hawk Weisman <hawk@meteorcodelabs.com>
2015-02-14 15:20:02 -0500

    add more axial trees

---
+++ src/main/scala/me/hawkweisman/lsystems/samples/AxialTrees.scala
@@ -0,0 +1,31 @@
+package me.hawkweisman.lsystems
+package samples
+
+import me.hawkweisman.lsystems.turtle._
+
+object AxialTree {
+  val rules = Map(
+    'F' -> Forward,
+    '-' -> TurnLeft,
+    '+' -> TurnRight,
+    '[' -> Push,
+    ']' -> Pop
+  ),
+}
+
+object AxialTreeA extends LSystem(
+  25.7f,
+  Map(
+    'F' -> "F[+F]F[-F]F"
+  ),
+  AxialTree.rules,
+  "F"
+)
+object AxialTreeB extends LSystem(
+  22.5f,
+  Map(
+    'F' -> "FF-[-F+F+F]+[+F-F-F]"
+  ),
+  AxialTree.rules,
+  "F"
+)
