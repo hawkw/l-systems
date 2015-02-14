92d0466b7bf2b5135ef50a61988ae13f5cee1515
Hawk Weisman <hawk@meteorcodelabs.com>
2015-02-14 14:42:52 -0500

    Sierpinski demo is now interactive

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -8,19 +8,19 @@ import PConstants._
 import PApplet._
 
 class Main extends PApplet {
-  var lastIter = SierpinskiLSystem.first
+  var lastIter = SierpinskiLSystem.first.next
   var steps = 0
-  val DIST = 2f
+  var dist = 25f
 
   def drawIter(it: LSystemIteration) = {
-    translate(0,height/4)
+    translate(width/2,height/2)
     steps += 5
     println(it.string.take(steps))
     for (ch <- it.string.take(steps)) {
       it.sys.commands(ch) match {
         case Forward =>
-          line(0,0,DIST,DIST)
-          translate(DIST,DIST)
+          line(0,0,dist,dist)
+          translate(dist,dist)
         case TurnLeft => rotate(radians(it.sys.angle))
         case TurnRight => rotate(radians(-it.sys.angle))
         case Push => pushMatrix()
@@ -31,14 +31,18 @@ class Main extends PApplet {
 
   override def setup {
     size(800, 800)
-    colorMode(HSB, 100)
     frameRate(999)
-    stroke(0);
+    stroke(255)
+    background(0)
   }
 
   override def draw: Unit = {
     drawIter(lastIter)
-    if (lastIter.generation < 10) lastIter = lastIter.next
+  }
+  override def mousePressed: Unit = {
+    background(0)
+    lastIter = lastIter.next
+    dist = dist/1.5f
   }
 }
 
